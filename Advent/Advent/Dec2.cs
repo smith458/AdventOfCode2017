8ee0cf80d86f6b9635b8639639b102c57d6e21ed
Unknown <steven.smith458@gmail.com>
2017-12-04 07:14:37 -0500

    Completed Day2

---
+++ Advent/Advent/Dec2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent
+{
+    public class Dec2
+    {
+        public static int CheckSum(Array arr)
+        {
+            int sum = 0;
+
+            foreach (int[] intArr in arr)
+            {
+                sum += intArr.Max() - intArr.Min();
+            }
+
+            return sum;
+        }
+
+        public static int CheckSumDiv(Array arr)
+        {
+            int sum = 0;
+
+            foreach (int[] intArr in arr)
+            {
+                sum += LookForDiv(intArr);
+            }
+
+            return sum;
+        }
+
+        public static int LookForDiv(int[] arr)
+        {
+            for (int x = 0; x < arr.Length; ++x)
+            {
+                for (int y = 0; y < arr.Length; ++y)
+                {
+                    if (arr[x] % arr[y] == 0 && x != y)
+                    {
+                        return arr[x] / arr[y];
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
