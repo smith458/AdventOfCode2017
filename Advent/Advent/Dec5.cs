d4904dadffff4de1fd65021a670aaf6db5ec48f7
Unknown <steven.smith458@gmail.com>
2017-12-05 21:21:27 -0500

    Day 5a method and tests done

---
+++ Advent/Advent/Dec5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent
+{
+    public class Dec5
+    {
+        public static int CountArraySteps(int[] steps)
+        {
+            int index = 0;      //Current index
+            int totalSteps = 0; //Total steps taken
+
+            while (index >= 0 && index < steps.Length)
+            {
+                index += steps[index]++; //Move and then increment the index after we move
+                totalSteps++;
+            }
+
+            return totalSteps;
+        }
+    }
+}
